dcb9e6c9c022f0c5eaafeac6417294dd0bdcfa7d
RWCannell <regancannell@gmail.com>
2024-02-16 18:48:32 +0200

    Removed public folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const path = require('path')
 const app = express();
-// app.use('/static', express.static(path.join(__dirname, 'public')));
-
-// app.use(express.static('public/assets'));
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
